a66d9574d655b4943e3b59ae2c8a2fb79ffa5fe3
Leila Mahammed <devmldesign@gmail.com>
2024-04-22 08:59:28 +0200

    ok test

---
+++ articles.php
@@ -0,0 +1,9 @@
+<?
+
+$articles = [
+    ["nom" => "expresso","prix" => 1],
+    ["nom" => "Chocolat Chaud","prix" => 1,50 ],
+    ["nom" => "Thé Citron","prix" => 1],
+];
+
+echo json_encode($articles);
